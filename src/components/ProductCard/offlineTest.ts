56195e377f7b40b0cf21313a8f4b9fedc007b419
Vasily Bobnev <ardxel@gmail.com>
2023-05-25 17:31:32 +0300

    Create offlineTest.ts

---
+++ src/components/ProductCard/offlineTest.ts
@@ -0,0 +1,11 @@
+import {IProductCard} from "@components/ProductCard/model.ts";
+
+export const offlineTest: IProductCard = {
+  "id": "64009b6b4aab67734e9cbcc1",
+  "seen": false,
+  "price": 120404.18,
+  "title": "REMOLD",
+  "address": "Floriston 261",
+  "about": "Tempor esse dolore culpa pariatur ut non culpa anim mollit duis tempor. Quis ipsum labore cillum nostrud tempor ullamco minim. Est cupidatat non nostrud aute dolor aliqua. Do fugiat commodo sit proident dolore deserunt mollit sunt. Dolore sunt enim voluptate duis dolore non laborum non minim aute exercitation. Laboris deserunt do sint ipsum. Id sint officia laboris cillum sint adipisicing est amet aliquip ullamco et nostrud.\r\n",
+  "createdAt": "2020-09-30T04:50:15 -05:00"
+}
